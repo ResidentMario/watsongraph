993f2fec0d8ff5fba42eb61516da9594b5aa3410
ResidentMario <aleksey.bilogur@gmail.com>
2015-12-26 14:01:04 -0500

    Refactor for publication.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'watson-graph',
+  packages = ['watson-graph'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Concept discovery and recommendation library built on top of the IBM Watson congitive API.',
+  author = 'Aleksey Bilogur',
+  author_email = 'aleksey.bilogur@gmail.com',
+  url = 'https://github.com/ResidentMario/watson-graph/tarball/0.1', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  keywords = ['graph', 'networks', 'ibm watson', 'ibm', 'recommendation'], # arbitrary keywords
+  classifiers = [],
+)
