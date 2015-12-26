3b6ee343534ebad3e15826c9e63e3a2106646c74
ResidentMario <aleksey.bilogur@gmail.com>
2015-12-26 15:36:18 -0500

    Increment again.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ setup(
   name = 'watsongraph',
   packages = ['watsongraph'], # this must be the same as the name above
   install_requires=['networkx', 'requests', 'mwviews'],
-  version = '0.1.2',
+  version = '0.1.3',
   description = 'Concept discovery and recommendation library built on top of the IBM Watson congitive API.',
   author = 'Aleksey Bilogur',
   author_email = 'aleksey.bilogur@gmail.com',
-  url = 'https://github.com/ResidentMario/watsongraph/tarball/0.1.2',
-  download_url = 'https://github.com/ResidentMario/watsongraph/tarball/0.1.2',
+  url = 'https://github.com/ResidentMario/watsongraph/tarball/0.1.3',
+  download_url = 'https://github.com/ResidentMario/watsongraph/tarball/0.1.3',
   keywords = ['graph', 'networks', 'ibm watson', 'ibm', 'recommendation'], # arbitrary keywords
   classifiers = [],
 )
