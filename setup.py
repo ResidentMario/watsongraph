c85122d41c1031e00983b3cb574d6f2868a2022b
ResidentMario <aleksey.bilogur@gmail.com>
2015-12-26 18:07:48 -0500

    Fix error.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
   name = 'watsongraph',
   packages = ['watsongraph'], # this must be the same as the name above
   install_requires=['networkx', 'requests', 'mwviews'],
-  version = '0.1.4',
+  version = '0.1.5',
   description = 'Concept discovery and recommendation library built on top of the IBM Watson congitive API.',
   author = 'Aleksey Bilogur',
   author_email = 'aleksey.bilogur@gmail.com',
