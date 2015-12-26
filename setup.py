9821d1578d33bf4a2cd52f1ea9c841ceaed2e995
ResidentMario <aleksey.bilogur@gmail.com>
2015-12-26 15:25:27 -0500

    Add install requirements to setup.

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from distutils.core import setup
 setup(
   name = 'watsongraph',
   packages = ['watsongraph'], # this must be the same as the name above
-  version = '0.1.1',
+  install_requires=['networkx, requests, mwviews'],
+  version = '0.1.0',
   description = 'Concept discovery and recommendation library built on top of the IBM Watson congitive API.',
   author = 'Aleksey Bilogur',
   author_email = 'aleksey.bilogur@gmail.com',
