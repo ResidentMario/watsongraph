029fd4e4f05506e9ceabffbd46b0eff5ef525a3e
ResidentMario <aleksey.bilogur@gmail.com>
2015-12-26 14:34:36 -0500

    More corrections.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'watsongraph',
   packages = ['watsongraph'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.1.1',
   description = 'Concept discovery and recommendation library built on top of the IBM Watson congitive API.',
   author = 'Aleksey Bilogur',
   author_email = 'aleksey.bilogur@gmail.com',
-  url = 'https://github.com/ResidentMario/watsongraph/tarball/0.1', # use the URL to the github repo
-  download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1', # I'll explain this in a second
+  url = 'https://github.com/ResidentMario/watsongraph/tarball/0.1.1', # use the URL to the github repo
+  download_url = 'https://github.com/ResidentMario/watson-graph/tarball/0.1.1', # I'll explain this in a second
   keywords = ['graph', 'networks', 'ibm watson', 'ibm', 'recommendation'], # arbitrary keywords
   classifiers = [],
 )
