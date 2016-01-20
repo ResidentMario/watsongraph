398abd682b7bc18907095db77a10fe8a3f686bf8
ResidentMario <aleksey.bilogur@gmail.com>
2016-01-20 12:07:56 -0500

    Re-configure path for readthedocs.

--- docs/conf.py
+++ docs/conf.py
@@ -15,17 +15,12 @@
 
 import sys
 import os
-from recommonmark.parser import CommonMarkParser
-
-
-sys.path.insert(0, "C:\Users\Alex\Desktop\watson-graph\watsongraph")
-sys.path.insert(0, "/Users/Honors/Desktop/watson-graph/watsongraph")
 
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..') + "/watsongraph")
 
 # -- General configuration ------------------------------------------------
 
